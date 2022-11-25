d6c24669a7c72fe36aeb7fd1612af67076071fc4
한성진 <trstyq@gmail.com>
2022-11-25 09:54:13 +0900

    Data Table에 이미지 Custon Type 생성해서 책 표지 나오도록 개발

--- src/lwc/bookPageLWC/bookPageLWC.js
+++ src/lwc/bookPageLWC/bookPageLWC.js
@@ -2,23 +2,72 @@
  * Created by 한성진 on 2022-11-24.
  */
 
-import { LightningElement, wire, api } from 'lwc';
+import {LightningElement, wire, api} from 'lwc';
 // wires the Apex method
 import getBook from '@salesforce/apex/AladinHomePageComponentController.getBook';
 
 const bookColumns =  [
-    { label: '제목', fieldName: 'name__c', type: 'text'},
-    { label: '구매여부', fieldName: 'ispurched__c', type: 'boolean'},
-    { label: '가격', fieldName: 'price__c', type: 'text'},
-    { label: '권', fieldName: 'volume__c', type: 'Number'},
-    { label: '표지', fieldName: 'cover__c', type: 'url'},
+    {label: '제목', fieldName: 'Name__c', type: 'text', editable: true},
+    {label: '구매여부', fieldName: 'IsPurchased__c', type: 'boolean', editable: true},
+    {label: '가격', fieldName: 'Price__c', type: 'text', editable: true},
+    {label: '권', fieldName: 'Volume__c', type: 'Number', editable: true},
+    {label: '표지', fieldName: 'Cover__c', type: 'image', editable: true},
+    {type: 'action', typeAttributes: { rowActions: actions }}
+];
+
+const actions = [
+    {label: 'Show details', name: 'show_details'},
+    {label: 'Delete', name: 'delete'}
 ];
 
 export default class BookPageLwc extends LightningElement {
 
-    @api recordId;
+    @api
+    recordId;
+
     bookColumns = bookColumns;
 
     @wire(getBook, {recordId : '$recordId'})
     books;
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case 'delete':
+                this.deleteRow(row);
+                break;
+            case 'show_details':
+                this.showRowDetails(row);
+                break;
+            default:
+        }
+    }
+
+    deleteRow(row) {
+        const { id } = row;
+        const index = this.findRowIndexById(id);
+        if (index !== -1) {
+            this.data = this.data
+                .slice(0, index)
+                .concat(this.data.slice(index + 1));
+        }
+    }
+
+    findRowIndexById(id) {
+        let ret = -1;
+        this.data.some((row, index) => {
+            if (row.id === id) {
+                ret = index;
+                return true;
+            }
+            return false;
+        });
+        return ret;
+    }
+
+    showRowDetails(row) {
+        this.record = row;
+    }
+
 }
--- src/lwc/helloWorld/helloWorld.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 한성진 on 2022-11-24.
- */
-
-import { LightningElement } from 'lwc';
-
-export default class HelloWorld extends LightningElement {
-
-}
